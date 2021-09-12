5bf66518ea9b6359f64c8a62dd1bf68c77c5f2bd
jasonc <jpacahela@gmail.com>
2021-09-12 17:08:12 -0500

    restyled landing page

--- frontend/src/components/LandingPage/index.js
+++ frontend/src/components/LandingPage/index.js
@@ -16,27 +16,27 @@ const LandingPage = () => {
 
   return (
     <div className="component-body">
-      <div className="landing-background-image">
-        <div className="landing-darkener">
-          <div className="landing-links-container">
+      <div className="landing-background-image"></div>
+        <div className="landing-darkener"></div>
+        <div className="landing-links-container">
             <div className="landing-container landing-login-container" >
               <LoginFormModal />
             </div>
             <div className="landing-container landing-signup-container">
               <NavLink to="/signup" id="landing-signup" >Sign Up</NavLink>
             </div>
-          </div>
-          <div className="landing-welcome-container">
-            <img className="landing-logo" src="/images/landing-logo.png" alt="Beer mug graphic" />
-            <h1 className="landing-welcome-title">BARHOPPR</h1>
-            <div className="landing-divider"></div>
-            <p className="landing-welcome-message">Share your favorite drinks with the world</p>
-            <div className="landing-welcome-signup-container" onClick={e => history.push('/signup')}>
-              <NavLink className="landing-welcome-signup" to="/signup" id="landing-signup" >Sign Up Here</NavLink>
-            </div>
+        </div>
+        <div className="landing-welcome-container">
+          <img className="landing-logo" src="/images/landing-logo.png" alt="Beer mug graphic" />
+          <h1 className="landing-welcome-title">BARHOPPR</h1>
+          <div className="landing-divider"></div>
+          <p className="landing-welcome-message">Share your favorite drinks with the world</p>
+          <div className="landing-welcome-signup-container" onClick={e => history.push('/signup')}>
+            <NavLink className="landing-welcome-signup" to="/signup" id="landing-signup" >Sign Up Here</NavLink>
           </div>
         </div>
-      </div>
+        
+      
     </div>
   )
 }
