3cc74ac3806d400f5e40910f34e92cdb1708ff66
jasonc <jpacahela@gmail.com>
2021-09-19 18:43:39 -0500

    changed prevHost if conditional to barhoppr.herokuapp.com

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -35,20 +35,20 @@ const LandingPage = () => {
       </div>
       <div className="landing-links-container">
           <LoginFormModal className="landing-login"/>
-          <div className={`landing-container landing-signup-container ${(window.location.pathname === '/') && (prevHost !== 'localhost') ? 'fadein-3' : ''}`}>
+          <div className={`landing-container landing-signup-container ${(window.location.pathname === '/') && (prevHost !== 'barhoppr.herokuapp.com') ? 'fadein-3' : ''}`}>
             <NavLink to="/signup" id="landing-signup" >Sign Up</NavLink>
           </div>
       </div>
       
-      <div className={`landing-welcome-container ${(window.location.pathname === '/') && (prevHost !== 'localhost') ? 'background-3' : ''}`}>
-        <img className={`landing-logo ${(window.location.pathname === '/') && (prevHost !== 'localhost') ? 'arrive-right-1' : ''}`} src="/images/landing-logo.png" alt="Beer mug graphic" />
-        <h1 className={`landing-welcome-title ${(window.location.pathname === '/') && (prevHost !== 'localhost') ? 'arrive-left-1' : ''}`}>BARHOPPR</h1>
-        <div className={`landing-divider ${(window.location.pathname === '/') && (prevHost !== 'localhost') ? 'arrive-up-1' : ''}`}></div>
-        <p className={`landing-welcome-message ${(window.location.pathname === '/') && (prevHost !== 'localhost') ? 'fadein-2' : ''}`}>Share your favorite drinks with the world!</p>
-        <div className={`landing-welcome-signup-container ${(window.location.pathname === '/') && (prevHost !== 'localhost') ? 'fadein-3' : ''}`} onClick={e => history.push('/signup')}>
+      <div className={`landing-welcome-container ${(window.location.pathname === '/') && (prevHost !== 'barhoppr.herokuapp.com') ? 'background-3' : ''}`}>
+        <img className={`landing-logo ${(window.location.pathname === '/') && (prevHost !== 'barhoppr.herokuapp.com') ? 'arrive-right-1' : ''}`} src="/images/landing-logo.png" alt="Beer mug graphic" />
+        <h1 className={`landing-welcome-title ${(window.location.pathname === '/') && (prevHost !== 'barhoppr.herokuapp.com') ? 'arrive-left-1' : ''}`}>BARHOPPR</h1>
+        <div className={`landing-divider ${(window.location.pathname === '/') && (prevHost !== 'barhoppr.herokuapp.com') ? 'arrive-up-1' : ''}`}></div>
+        <p className={`landing-welcome-message ${(window.location.pathname === '/') && (prevHost !== 'barhoppr.herokuapp.com') ? 'fadein-2' : ''}`}>Share your favorite drinks with the world!</p>
+        <div className={`landing-welcome-signup-container ${(window.location.pathname === '/') && (prevHost !== 'barhoppr.herokuapp.com') ? 'fadein-3' : ''}`} onClick={e => history.push('/signup')}>
           <NavLink className="landing-welcome-signup" to="/signup" id="landing-signup" >Sign Up Here</NavLink>
         </div>
-        <NavLink onClick={getDemoUser} id={(window.location.pathname === '/') && (prevHost !== 'localhost') ? 'landing-welcome-demo' : 'landing-welcome-demo-no-animation'} to="/bar-talk" >Log in as a demo user</NavLink>
+        <NavLink onClick={getDemoUser} id={(window.location.pathname === '/') && (prevHost !== 'barhoppr.herokuapp.com') ? 'landing-welcome-demo' : 'landing-welcome-demo-no-animation'} to="/bar-talk" >Log in as a demo user</NavLink>
 
       </div>
         
